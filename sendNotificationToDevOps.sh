2aa9dbed2a846e633ff320ef0655bb290eac37af
rally1-rallydev <74136437+rally1-rallydev@users.noreply.github.com>
2020-11-19 19:25:34 +0200

    Update sendNotificationToDevOps.sh

--- sendNotificationToDevOps.sh
+++ sendNotificationToDevOps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export DEVOPS_SERVER_URL=https://cddirector.io
+export DIL_RELEASE_DSL_FILENAME=release.json
+
 export DEVOPS_TENANT_ID="17827b55-3a42-403f-b1f8-9655d7e625bb"
 export DEVOPS_API_KEY="eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6InJhbGx5MS5yYWxseWRldkBnbWFpbC5jb20iLCJ0ZW5hbnRJZCI6IjE3ODI3YjU1LTNhNDItNDAzZi1iMWY4LTk2NTVkN2U2MjViYiIsInVzZXJJZCI6MSwianRpIjoiMTM1NmQ3YzItMzdhYi00ZTRlLTk1ZWItYTJmZGVlMjgzMzhlIiwiZXhwIjoxNjEyNjA4MjgwfQ.WUVAR0J2lPG4hiOmx486tEmMw_zkksFNqM6NbEtKXe1pduJXSBNbZXdcgFVcuGPz_3qqaYYVs6qqJWB1AT1e6g"
 
@@ -8,12 +10,12 @@ export DEVOPS_API_KEY="eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6InJhbGx5MS5yYWxseWRl
 
 export GIT_URL=https://github.com/rally1-rallydev/code-churn.git
 export GIT_BRANCH=main
-export GIT_COMMIT=ca7f937abdf97ae18a75253c279dd167fdff527d
-export GIT_PREVIOUS_SUCCESSFUL_COMMIT=ca7f937abdf97ae18a75253c279dd167fdff527d
+export GIT_COMMIT=77e9fe0823f22e368cf398476660570f6f84e2d9
+export GIT_PREVIOUS_SUCCESSFUL_COMMIT=77e9fe0823f22e368cf398476660570f6f84e2d9
 
 export DIL_REPOSITORY_NAME=`echo $GIT_URL | sed 's/^.*\/\(.*\).git$/\1/'`
 export DIL_REPOSITORY_BRANCH=$GIT_BRANCH
 export DIL_REPOSITORY_BRANCH_GIT_COMMIT_ID=$GIT_COMMIT
 export DIL_REPOSITORY_BRANCH_BUILD_NUMBER=$GIT_PREVIOUS_SUCCESSFUL_COMMIT
 
-curl -s --header "Content-Type: application/json" --header "Accept: application/json" -d "{ \"applicationName\": \"$DIL_REPOSITORY_NAME\", \"applicationVersionBuildNumber\": \"$DIL_REPOSITORY_BRANCH_BUILD_NUMBER\", \"applicationVersionName\": \"$DIL_REPOSITORY_BRANCH\", \"commits\": [ { \"commitId\": \"$DIL_REPOSITORY_BRANCH_GIT_COMMIT_ID\" } ]}" "$DEVOPS_SERVER_URL/cdd/design/$DEVOPS_TENANT_ID/v1/applications/application-versions/application-version-builds" -H "Authorization: Bearer $DEVOPS_API_KEY"
+curl -s --header "Content-Type: application/json" --header "Accept: application/json" -d "{ \"applicationName\": \"$DIL_REPOSITORY_NAME\", \"applicationVersionBuildNumber\": \"$DIL_REPOSITORY_BRANCH_BUILD_NUMBER\", \"applicationVersionName\": \"$DIL_REPOSITORY_BRANCH\", \"commits\": [ { \"commitId\": \"$DIL_REPOSITORY_BRANCH_GIT_COMMIT_ID\" } ], \"releaseDsl\": { \"dslFilename\": \"$DIL_RELEASE_DSL_FILENAME\", \"dslParameters\": { \"DIL_REPOSITORY_BRANCH\": \"${DIL_REPOSITORY_BRANCH}\" }, \"fileSourceName\": \"$DIL_REPOSITORY_NAME\", \"fileSourceParameters\": {}, \"releaseDslScope\": \"APPLICATION\" } }" "$DEVOPS_SERVER_URL/cdd/design/$DEVOPS_TENANT_ID/v1/applications/application-versions/application-version-builds" -H "Authorization: Bearer $DEVOPS_API_KEY"
